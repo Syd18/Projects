18ef36cf975170aabb4704cb8a7c002766a10468
Syed Shahed <97220584+Syd18@users.noreply.github.com>
2024-02-01 12:51:42 +0600

    Delete Optimization directory

--- Optimization/MATLAB/initial_population.m
+++
@@ -1,8 +0,0 @@
-function [population]=initial_population(lower_bound,upper_bound,population_size,variables)
-population=zeros(population_size,variables);
-
-%Initial Population
-for i=1:variables
-population(:,i)=lower_bound(i)+(upper_bound(i)-lower_bound(i))*rand(population_size,1);
-end
-end
--- Optimization/MATLAB/trimmer.m
+++
@@ -1,8 +0,0 @@
-function[z]=trimmer(lower_bound,upper_bound,population)
-[row,col]=size(population);
-for i=1:col
-population(population(:,i)<lower_bound(i),i)=lower_bound(i);
-population(population(:,i)>upper_bound(i),i)=upper_bound(i);
-end
-z=population;
-end
